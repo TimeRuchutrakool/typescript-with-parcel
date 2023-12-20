8f4076b66e8e94cb2638ccf2bd374408df418eb1
TimeRuchutrakool <timeruchu0808@gmail.com>
2023-12-20 09:44:50 +0700

    1

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
+//ใช้ตอนที่ key ใน object มัน unknown to us -> เราไม่รู้ pattern ของ key prop
+
 type Airplane = {
-  flightNumber: string;
-  airplaneModel: string;
-  dateOfDeparture: string;
-  timeOfDeparture: string;
+  // flightNumber: string;
+  // airplaneModel: string;
+  // dateOfDeparture: string;
+  // timeOfDeparture: string;
   from: string;
   to: string;
   seats: {
-    [key: string]: string;
+    [k: string]: string; //valid เมื่อ key เป็น string และ value เป็น string
   };
 };
+
+const airplane: Airplane = {
+  from: 'A',
+  to: 'B',
+  seats: {
+    a:'A',b:'B'
+  }
+}
