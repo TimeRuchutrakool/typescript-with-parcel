3821852dd07b90f934f46028eab2d899693a5db7
TimeRuchutrakool <timeruchu0808@gmail.com>
2023-12-20 15:01:12 +0700

    1

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,36 @@ type Reservation = {
   destination: string;
 };
 
+// type Reserve1 = {
+//   (
+//     departureDate: Date,
+//     returnDate: Date,
+//     departingFrom: string,
+//     destination: string
+//   ): Reservation; //! มี 2 signature ละ // มันจะเอา 4 หรือ 3 ตอนไปสร้าง function -> มันเอา ตำแหน่งมา combine กัน คือ ตัวแรก โอเค ตัวที่ 2 คือ returnDate ที่เป็น string กับ departingForm ที่เป็น string -> ดังนั้นตัวที่ 2 ต้องเป็น Date | string
+//   (
+//    departureDate: Date,
+//    departingFrom: string,
+//    destination: string
+//    ): Reservation
+// };
+
+// const reserve1: Reserve1 = (
+//   departureDate,
+//   returnDateOrDepartingFrom,
+//   departingFromOrDestination,
+//   destination
+// ) => {
+  
+// }
+
 type Reserve = {
   (
     departureDate: Date,
     returnDate: Date,
     departingFrom: string,
     destination: string
-  ): Reservation | never;
+  ): Reservation | never; //บอกว่า can throw an error
   (departureDate: Date, departingFrom: string, destination: string):
     | Reservation
     | never;
