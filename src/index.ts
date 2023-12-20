036ae3a1d8d317133ec77906cfd93f44a52a1e7f
TimeRuchutrakool <timeruchu0808@gmail.com>
2023-12-20 16:10:08 +0700

    1

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
+
+const mapT = <T>(array: T[], callback: (item: T) => T): T[] => { //ต้องมี another generic เพราะสมมติรับมาเป็น number เอามาแปลงเป็น string มันจะ returnเป็น type เดียวกันไม่ได้
+  if(array.length===0) return array
+  const newArray = []
+  for (const item of array) {
+    const newItem = callback(item)
+    newArray.push(newItem)
+  }
+  return newArray
+}
+
 const map = <T, U>(array: T[], func: (item: T) => U) => {
   // check if the array is empty
   if (array.length === 0) {
