cc93dbf3b1f1d7a6a93971d97b94c701a92eba9e
TimeRuchutrakool <timeruchu0808@gmail.com>
2023-12-20 14:43:42 +0700

    1

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,16 @@ type Reservation = {
   destination: string;
 };
 
+// type Reserve = ()=>{} //ใช้ parameter เป็รน Reservation prop เพื่อที่จะเอามาสร้าง object
+
+// type Reserve =  (
+//     departureDate: Date,
+//     returnDate: Date,
+//     departingFrom: string,
+//     destination: string
+//   ) => Reservation; //จะให้ return Reservation มาให้เรา
+
+// เป็น function call signature ที่ รับ parameter 4 ตัว แล้ว return Reservation type
 type Reserve = {
   (
     departureDate: Date,
